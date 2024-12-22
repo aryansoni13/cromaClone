d323096bf4a1a8bc1af2358aa792224b39d0d764
ARYAN SONI <arusoni477@gmail.com>
2024-12-23 00:22:05 +0530

    added few files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import LandingPage from "./Components/Landing/Landing";
+import Cart from "./Components/Cart/Cart";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+// 2 ways - vite , create-react-app
